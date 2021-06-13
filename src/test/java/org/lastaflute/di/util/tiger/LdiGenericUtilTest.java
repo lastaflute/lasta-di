372463001d630a39bc0e99ed826d3772455a9af6
jflute <jflute@localhost>
2021-06-13 23:56:44 +0900

    use UTFlute core for PlainTestCase

---
+++ src/test/java/org/lastaflute/di/util/tiger/LdiGenericUtilTest.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2015-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.lastaflute.di.util.tiger;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Type;
+import java.util.Map;
+
+import org.dbflute.utflute.core.PlainTestCase;
+
+/**
+ * @author jflute
+ */
+public class LdiGenericUtilTest extends PlainTestCase {
+
+    public void test_getGenericFirstClass_returnType_basic() throws Exception {
+        // ## Arrange ##
+        Method declaredMethod = Sea.class.getDeclaredMethod("dockside", (Class<?>[]) null);
+        Type genericReturnType = declaredMethod.getGenericReturnType();
+
+        // ## Act ##
+        // ## Assert ##
+        log("return: exp={}", genericReturnType);
+        assertEquals(String.class, LdiGenericUtil.getGenericFirstClass(genericReturnType));
+        assertEquals(Integer.class, LdiGenericUtil.getGenericSecondClass(genericReturnType));
+
+        Type[] genericParameterTypes = LdiGenericUtil.getGenericParameterTypes(genericReturnType);
+        assertEquals(2, genericParameterTypes.length);
+        Type firstParameterType = genericParameterTypes[0];
+        log("firstParameterType: type={}", firstParameterType.getClass());
+        log("firstParameterType: exp={}", firstParameterType.toString());
+        assertEquals("class java.lang.String", firstParameterType.toString());
+
+        Type[] nestedTypes = LdiGenericUtil.getGenericParameterTypes(firstParameterType);
+        assertEquals(0, nestedTypes.length);
+    }
+
+    public void test_getGenericFirstClass_returnType_nested() throws Exception {
+        // ## Arrange ##
+        Method declaredMethod = Sea.class.getDeclaredMethod("hangar", (Class<?>[]) null);
+        Type genericReturnType = declaredMethod.getGenericReturnType();
+
+        // ## Act ##
+        // ## Assert ##
+        log("return: exp={}", genericReturnType);
+        assertEquals(Map.class, LdiGenericUtil.getGenericFirstClass(genericReturnType));
+        assertNull(LdiGenericUtil.getGenericSecondClass(genericReturnType));
+
+        Type[] genericParameterTypes = LdiGenericUtil.getGenericParameterTypes(genericReturnType);
+        assertEquals(1, genericParameterTypes.length);
+        Type firstParameterType = genericParameterTypes[0];
+        log("firstParameterType: type={}", firstParameterType.getClass());
+        log("firstParameterType: exp={}", firstParameterType.toString());
+        assertEquals("java.util.Map<java.lang.String, java.lang.Integer>", firstParameterType.toString());
+        assertEquals(String.class, LdiGenericUtil.getGenericFirstClass(firstParameterType));
+        assertEquals(Integer.class, LdiGenericUtil.getGenericSecondClass(firstParameterType));
+
+        Type[] nestedTypes = LdiGenericUtil.getGenericParameterTypes(firstParameterType);
+        assertEquals(2, nestedTypes.length);
+    }
+
+    public static class Sea {
+        public Dockside<String, Integer> dockside() {
+            return null;
+        }
+
+        public Hangar<Map<String, Integer>> hangar() {
+            return null;
+        }
+    }
+
+    public static class Dockside<A, B> {
+    }
+
+    public static class Hangar<A> {
+    }
+}
